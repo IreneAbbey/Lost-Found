c1acd73f8387022920238489861e73bc7ddf78ae
Irene Abbey <abbeyirene09@gmail.com>
2025-03-21 16:38:37 +0000

    feat:Successfully created LostItem model

--- api/models.py
+++ api/models.py
@@ -2,15 +2,15 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-class Users(AbstractUser):
-    ROLE_CHOICES = [
+class User(AbstractUser):
+    ROLE = [
         ('admin', 'admin'),
         ('passenger', 'Passenger'),
         ('driver', 'Driver'),
     ]
     email = models.EmailField(unique= True),
     phone_number = models.CharField(max_length= 10),
-    role = models.CharField(max_length= 10, choices=ROLE_CHOICES, default='passenger')
+    role = models.CharField(max_length= 10, choices=ROLE, default='passenger')
 
     groups =  models.ManyToManyField(
         'auth.Group',
@@ -26,3 +26,21 @@ class Users(AbstractUser):
 
     def __str__(self):
         return self.username
+
+class LostItem(models.Model):
+    STATUS = [
+        ('pending', 'Pending'),
+        ('matched', 'Matched'),
+        ('recorved', 'Recovered')
+    ]
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    item_name = models.CharField(max_length=200),
+    description = models.TextField(),
+    date_lost = models.DateTimeField(null=True),
+    location = models.CharField(max_length=200),
+    vehicle_type = models.CharField(max_length=100),
+    vehicle_description = models.TextField(),
+    status = models.CharField(max_length=10, choices=STATUS, default='pending')
+
+    def __str__(self):
+        return self.item_name
